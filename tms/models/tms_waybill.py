77effe753439b059ee506446c71e539a9fcb70b0
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2022-01-03 09:59:01 -0600

    [ADD] tms: show smart button in waybill to view related invoice

--- tms/models/tms_waybill.py
+++ tms/models/tms_waybill.py
@@ -224,6 +224,36 @@ class TmsWaybill(models.Model):
         for waybill in self:
             waybill.state = 'approved'
 
+    def action_view_invoice(self):
+        invoices = self.mapped('invoice_id')
+        action = self.env["ir.actions.actions"]._for_xml_id("account.action_move_out_invoice_type")
+        if len(invoices) > 1:
+            action['domain'] = [('id', 'in', invoices.ids)]
+        elif len(invoices) == 1:
+            form_view = [(self.env.ref('account.view_move_form').id, 'form')]
+            if 'views' in action:
+                action['views'] = form_view + [(state,view) for state,view in action['views'] if view != 'form']
+            else:
+                action['views'] = form_view
+            action['res_id'] = invoices.id
+        else:
+            action = {'type': 'ir.actions.act_window_close'}
+
+        context = {
+            'default_move_type': 'out_invoice',
+        }
+        if len(self) == 1:
+            context.update({
+                'default_partner_id': self.partner_id.id,
+                'default_partner_shipping_id': self.arrival_address_id.id,
+                'default_invoice_payment_term_id': self.partner_id.property_payment_term_id.id or self.env[
+                    'account.move'].default_get(['invoice_payment_term_id']).get('invoice_payment_term_id'),
+                'default_invoice_origin': self.name,
+                'default_user_id': self.user_id.id,
+            })
+        action['context'] = context
+        return action
+
     @api.depends('invoice_id')
     def _compute_invoice_paid(self):
         for rec in self:
