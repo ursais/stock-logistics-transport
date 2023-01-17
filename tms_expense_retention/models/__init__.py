c30ebdff72ca4f9230fd23adc9194193d9ea2fd8
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2023-01-16 22:46:30 -0600

    [REM] tms*: remove unused modules

--- tms_expense_retention/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2016, Jarsa Sistemas, S.A. de C.V.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-from . import product_template
-from . import tms_retention
-from . import tms_expense
--- tms_tollstations/wizards/__init__.py
+++
@@ -1,5 +0,0 @@
-# Copyright 2012, Israel Cruz Argil, Argil Consulting
-# Copyright 2016, Jarsa Sistemas, S.A. de C.V.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
-
-from . import tms_toll_import
