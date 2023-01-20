20b158e65b4c88543a911f40eac2166a23d277a6
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2023-01-20 11:03:06 -0600

    [REF] tms: change licence

--- tms/models/hr_employee.py
+++ tms/models/hr_employee.py
@@ -1,5 +1,5 @@
 # Copyright 2016-2023, Jarsa Sistemas, S.A. de C.V.
-# # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
--- tms/models/tms_place.py
+++ tms/models/tms_place.py
@@ -1,5 +1,4 @@
-# Copyright 2012, Israel Cruz Argil, Argil Consulting
-# Copyright 2016, Jarsa Sistemas, S.A. de C.V.
+# Copyright 2016-2023, Jarsa Sistemas, S.A. de C.V.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 from odoo import api, fields, models
