fb91c9cb7dbf7318a6036af0f854985d55586507
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com>
2023-01-31 14:58:12 -0600

    [ADD] tms: add waybill id field in account.move

--- tms/models/account_move.py
+++ tms/models/account_move.py
@@ -1,7 +1,19 @@
 # Copyright 2016-2023, Jarsa Sistemas, S.A. de C.V.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo import fields, models
+from odoo import api, fields, models
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    waybill_id = fields.Many2one("tms.waybill", compute="_compute_waybill_id", store=True)
+
+    @api.depends("line_ids.waybill_id")
+    def _compute_waybill_id(self):
+        for rec in self:
+            if rec.line_ids and rec.line_ids[0].waybill_id:
+                rec.waybill_id = rec.line_ids.waybill_id
 
 
 class AccountMoveLine(models.Model):
