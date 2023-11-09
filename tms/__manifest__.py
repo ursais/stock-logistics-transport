6e51e0f2070e6030d9255aa63a8baee0b9735739
Luis Torres <luis_t@vauxoo.com>
2023-11-09 16:13:22 +0000

    [FIX] tms: Removed incorrect widget for date field
    Avoid traceback error when try to open an unit created (fleet.vehicle)

--- tms/__manifest__.py
+++ tms/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Freight Management",
-    "version": "15.0.2.0.4",
+    "version": "15.0.2.0.5",
     "category": "Transport",
     "author": "Jarsa",
     "website": "https://www.jarsa.com.mx/page/transport-management-system",
