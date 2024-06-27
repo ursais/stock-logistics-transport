9fc8cec9192701946d9e399f3784e79a02c23ed9
Santiago Rodriguez <santiromur@gmail.com>
2024-06-27 13:38:25 -0600

    [ADD] tms: Add initial directories and setup

---
+++ tms/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright (C) 2018 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Transportation Management System",
+    "summary": "Manage Vehicles, Routes and Delivers",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "category": "Transportation Management System",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-transport",
+    "depends": ["base"],
+    "data": [],
+    "demo": [],
+    "application": True,
+    "development_status": "Alpha",
+    "maintainers": ["max3903", "santiagordz"],
+    "assets": {},
+}
